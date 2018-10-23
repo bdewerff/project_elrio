e0dd57dde5a7e1c942bbf31a90df71d7f1a341fa
Brett Dewerff <brett.dewerff@my.simpson.edu>
2018-10-23 09:19:06 -0500

    Changed to PHP Files

---
+++ public_html/navigation.php
@@ -0,0 +1,36 @@
+<!-- Navigation Bar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			    <a class="navbar-brand" href="#">El Rio</a>
+			    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"                                  aria-expanded="false" aria-label="Toggle navigation">
+			        <span class="navbar-toggler-icon"></span>
+			    </button>
+
+			    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+			        <ul class="navbar-nav mr-auto">
+			            <li class="nav-item active">
+			                <a class="nav-link" href="index.php">Home Page<span class="sr-only">(current)</span></a>
+			            </li>
+
+			            <li class="nav-item">
+			                <a class="nav-link" href="about.php">About Us</a>
+			            </li>
+			            
+			        <li class="nav-item dropdown">
+			           
+			            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			                Menu
+			            </a>
+
+			            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			            <a class="dropdown-item" href="breakfast.php">Breakfast</a>
+			                <a class="dropdown-item" href="dinner.php">Dinner</a>
+			                <div class="dropdown-divider"></div>
+			            <a class="dropdown-item" href="drinks.php">Drinks</a>
+			            <a class="dropdown-item" href="dessert.php">Dessert</a>
+			            </div>
+			        </li>
+			    </ul>
+
+			    </div>
+			</nav>
